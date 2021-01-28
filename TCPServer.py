72a8fc4a80898c2915cce4d109e741edece93f80
forcallinghenney <godisbest101@gmail.com>
2021-01-28 22:28:23 +0900

    refactored request handler

--- TCPServer.py
+++ TCPServer.py
@@ -1,19 +1,23 @@
-import socketserver, threading, Setting
+import Setting
+from Data import DBConnector, RawDataCollection
+
+import socketserver, threading
 from struct import calcsize
 import datastream_processes as dp
-from DataList import DataList
-from DBConnector import DBConnector
 
 HEADERS = {'RECORD': b'RECORD', 'SHUTDOWN': b'SHUTDW', 'SAVE START': b'SVSTRT', 'SAVE STOP': b'SVSTOP'}
 MAX_LENGTH = 2048
+LOCK = threading.Lock()
 
 
 def threads_join():
     for thread in threading.enumerate():
-        if thread not in [threading.main_thread(), threading.current_thread()]:
-            # print(thread.getName(), 'wait for close')
+        name = thread.getName()
+
+        if name.startswith("Thread-") and thread is not threading.current_thread():
+            # print(name, 'wait for close')
             thread.join()
-            # print(thread.getName(), 'is closed')
+            # print(name, 'is closed')
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -21,60 +25,66 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-
     def handle(self):
         msg_header = self.request.recv(calcsize('6s'))
 
-        if msg_header == HEADERS['SHUTDOWN']:
-            self.server.shutdown()
-            DBConnector.instance().close()
-            threads_join()
-
-            print('\nshutdown server')
-            return
+        process = {HEADERS['RECORD']: self.record_save_process,
+                   HEADERS['SHUTDOWN']: self.shutdown_process,
+                   HEADERS['SAVE START']: self.save_start_process,
+                   HEADERS['SAVE STOP']: self.save_stop_process}.get(msg_header, self.shutdown_process)
 
         if msg_header not in HEADERS.values():
-            print('Incorrect header is given: {}'.format(msg_header))
-            return
+            process(msg_header)
+        else:
+            process()
 
-        if msg_header == HEADERS['SAVE STOP']:
-            self.server.shutdown()
-            print('\nshutdown to stop saving')
+    # Corresponding Processes
+    def record_save_process(self):
+        buffer = self.request.recv(MAX_LENGTH)
+        timestamp, device_id, records = dp.parse_datastream(buffer)
 
-            threads_join()
-            Setting.SAVE = False
+        if len(records) == 1 and Setting.SAVE:
+            LOCK.acquire()
+            try:
+                RawDataCollection.instance().add(timestamp, device_id, records[Setting.COLLECTING_DEVICE_MAC[0]])
+            finally:
+                LOCK.release()
 
-            # save to database
-            # DataList.instance().print()
-            DBConnector.instance().insert(DataList.instance().get())
-            DataList.instance().remove_all()
+    def shutdown_process(self, incorrect_header=None):
+        self.server.shutdown()
+        DBConnector.instance().close()
+        threads_join()
 
-            print('reopen server after saving to database at ', threading.current_thread().getName())
-            self.server.serve_forever()
+        if incorrect_header:
+            print("\nIncorrect header is given: ", incorrect_header, end="")
+        print('\nshutdown:: shutdown_process')
 
-        elif msg_header == HEADERS['SAVE START']:
-            self.server.shutdown()
-            print('\nshutdown to start saving')
+    def save_start_process(self):
+        self.server.shutdown()
+        print('\nshutdown:: save_start_process')
 
-            coordinate = self.request.recv(10).decode()
-            x, y = coordinate.split(',')
-            print(x, y)
-            DBConnector.instance().set_coordinate(x, y)
+        threads_join()
+        Setting.SAVE = True
 
-            threads_join()
-            Setting.SAVE = True
+        coordinate = self.request.recv(10).decode()
+        x, y = coordinate.split(',')
+        print("Coordinate: ", x, y)
+        DBConnector.instance().set_coordinate(x, y)
 
-            print('reopen with start saving at ', threading.current_thread().getName())
-            self.server.serve_forever()
+        print('reopen server at %s:: save_start_process' % threading.current_thread().getName())
+        self.server.serve_forever()
 
-        elif msg_header == HEADERS['RECORD']:
-            buffer = self.request.recv(MAX_LENGTH)
-            timestamp, device_id, records = dp.parse_datastream(buffer)
+    def save_stop_process(self):
+        self.server.shutdown()
+        print('\nshutdown:: save_stop_process')
 
-            if len(records) == 1 and Setting.SAVE:
-                Setting.LOCK.acquire()
-                try:
-                    dp.save_data(timestamp, device_id, records)
-                finally:
-                    Setting.LOCK.release()
+        threads_join()
+        Setting.SAVE = False
 
+        # save to database
+        # RawDataCollection.instance().print()
+        DBConnector.instance().insert(RawDataCollection.instance().get())
+        RawDataCollection.instance().remove_all()
+
+        print('reopen server at %s:: save_stop_process' % threading.current_thread().getName())
+        self.server.serve_forever()
